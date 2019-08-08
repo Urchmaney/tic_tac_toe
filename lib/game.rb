246107cad99553731a6af2f7f28a89f42da98de3
fatima ahmed <fatymah001@gmail.com>
2019-08-08 22:33:18 +0300

    fix syntax error

--- lib/game.rb
+++ lib/game.rb
@@ -7,14 +7,14 @@ class Game
   end
 
   def check_for_win
-      if @board[0][0] == @board[1][0] == @board[2][0] || #left
-          @board[0][0] == @board[1][1] == @board[2][2] || # left Diagonal
-          @board[0][1] == @board[1][1] == @board[2][1] || #middle vertical
-          @board[0][2] == @board[1][2] == @board[2][2] || #right
-          @board[1][0] == @board[1][1] == @board[1][2] || # middle horizontal
-          @board[2][0] == @board[2][1] == @board[2][2] || #down
-          @board[0][0] == @board[0][1] == @board[0][2] || #top
-          @board[0][2] == @board[1][1] == @board[2][0]  #righr diagonal
+      if ( @board[0][0] == @board[1][0] ) && ( @board[1][0] == @board[2][0] ) || #left
+         ( @board[0][0] == @board[1][1] ) && ( @board[1][1] == @board[2][2] ) || # left Diagonal
+         ( @board[0][1] == @board[1][1] ) && ( @board[1][1] == @board[2][1] ) || #middle vertical
+         ( @board[0][2] == @board[1][2] ) && ( @board[1][2] == @board[2][2] ) || #right
+         ( @board[1][0] == @board[1][1] ) && ( @board[1][1] == @board[1][2] ) || # middle horizontal
+         ( @board[2][0] == @board[2][1] ) && ( @board[2][1] == @board[2][2] ) || #down
+         ( @board[0][0] == @board[0][1] ) && ( @board[0][1] == @board[0][2] ) || #top
+         ( @board[0][2] == @board[1][1] ) && ( @board[1][1] == @board[2][0] ) #right diagonal
           
           return true
       end
@@ -32,3 +32,5 @@ class Game
   
   
 end
+game=Game.new()
+puts game.check_for_win
