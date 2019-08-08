cf1b97d1b795b3bba1e3932be9b7c8adea2d2453
Urchmaney <kingsobino@gmail.com>
2019-08-08 18:05:04 +0000

    Implement the check for win method

---
+++ lib/game.rb
@@ -0,0 +1,34 @@
+class Game
+
+  def initialize
+    @board = [["_", "_", "_"], ["_", "_", "_"], ["_", "_", "_"]]
+    @player = 1
+    @space_used = 0
+  end
+
+  def check_for_win
+      if @board[0][0] == @board[1][0] == @board[2][0] || #left
+          @board[0][0] == @board[1][1] == @board[2][2] || # left Diagonal
+          @board[0][1] == @board[1][1] == @board[2][1] || #middle vertical
+          @board[0][2] == @board[1][2] == @board[2][2] || #right
+          @board[1][0] == @board[1][1] == @board[1][2] || # middle horizontal
+          @board[2][0] == @board[2][1] == @board[2][2] || #down
+          @board[0][0] == @board[0][1] == @board[0][2] || #top
+          @board[0][2] == @board[1][1] == @board[2][0]  #righr diagonal
+          
+          return true
+      end
+      return false
+      
+  end
+
+  def display_board
+
+  end
+
+  def all_space_used
+
+  end
+  
+  
+end
