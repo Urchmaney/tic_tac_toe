69e5bfb5a4f396918c714c0f5c1915de8bfdf88e
fatima ahmed <fatymah001@gmail.com>
2019-08-08 22:49:48 +0300

    fix for condition size is too high

--- lib/game.rb
+++ lib/game.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class Game
+class Game #:nodoc:
   def initialize
     @board = [%w[_ _ _], %w[_ _ _], %w[_ _ _]]
     @player = 1
@@ -8,17 +8,31 @@ class Game
   end
 
   def check_for_win
-    if (@board[0][0] == @board[1][0]) && (@board[1][0] == @board[2][0]) || # left
-      (@board[0][0] == @board[1][1]) && (@board[1][1] == @board[2][2]) || # left Diagonal
-      (@board[0][1] == @board[1][1]) && (@board[1][1] == @board[2][1]) || # middle vertical
-      (@board[0][2] == @board[1][2]) && (@board[1][2] == @board[2][2]) || # right
-      (@board[1][0] == @board[1][1]) && (@board[1][1] == @board[1][2]) || # middle horizontal
-      (@board[2][0] == @board[2][1]) && (@board[2][1] == @board[2][2]) || # down
-      (@board[0][0] == @board[0][1]) && (@board[0][1] == @board[0][2]) || # top
-      (@board[0][2] == @board[1][1]) && (@board[1][1] == @board[2][0]) # right diagonal
+    if (@board[0][0] == @board[1][0]) && (@board[1][0] == @board[2][0]) # left
       return true
     end
-    false 
+    if (@board[0][0] == @board[1][1]) && (@board[1][1] == @board[2][2]) # left Diagonal
+      return true
+    end
+    if (@board[0][1] == @board[1][1]) && (@board[1][1] == @board[2][1]) || # middle vertical
+      return true
+    end
+    if (@board[0][2] == @board[1][2]) && (@board[1][2] == @board[2][2]) || # right
+      return true
+    end
+    if (@board[1][0] == @board[1][1]) && (@board[1][1] == @board[1][2]) || # middle horizontal
+      return true
+    end
+    if (@board[2][0] == @board[2][1]) && (@board[2][1] == @board[2][2]) || # down
+      return true
+    end
+    if (@board[0][0] == @board[0][1]) && (@board[0][1] == @board[0][2]) || # top
+      return true
+    end
+    if (@board[0][2] == @board[1][1]) && (@board[1][1] == @board[2][0]) # right diagonal
+      return true
+    end
+    false
   end
 
   def display_board
