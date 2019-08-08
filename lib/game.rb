e5487190a6c48fcb18641106003163ee920891e7
Urchmaney <kingsobino@gmail.com>
2019-08-08 20:17:12 +0000

    Linter work on the max condition error

--- lib/game.rb
+++ lib/game.rb
@@ -8,31 +8,35 @@ class Game #:nodoc:
   end
 
   def check_for_win
-    if (@board[0][0] == @board[1][0]) && (@board[1][0] == @board[2][0]) # left
+    if check_equality(@board[0][0], @board[1][0], @board[2][0]) # left
       return true
     end
-    if (@board[0][0] == @board[1][1]) && (@board[1][1] == @board[2][2]) # left Diagonal
+    if check_equality(@board[0][0], @board[1][1], @board[2][2]) # left Diagonal
       return true
     end
-    if (@board[0][1] == @board[1][1]) && (@board[1][1] == @board[2][1]) # middle vertical
+    if check_equality(@board[0][1], @board[1][1], @board[2][1]) # middle vertical
       return true
     end
-    if (@board[0][2] == @board[1][2]) && (@board[1][2] == @board[2][2]) # right
+    if check_equality(@board[0][2], @board[1][2], @board[2][2]) # right
       return true
     end
-    if (@board[1][0] == @board[1][1]) && (@board[1][1] == @board[1][2]) # middle horizontal
+    if check_equality(@board[1][0], @board[1][1], @board[1][2) # middle horizontal
       return true
     end
-    if (@board[2][0] == @board[2][1]) && (@board[2][1] == @board[2][2]) # down
+    if check_equality(@board[2][0], @board[2][1], @board[2][2]) # down
       return true
     end
-    if (@board[0][0] == @board[0][1]) && (@board[0][1] == @board[0][2]) # top
+    if check_equality(@board[0][0], @board[0][1], @board[0][2]) # top
       return true
     end
-    if (@board[0][2] == @board[1][1]) && (@board[1][1] == @board[2][0]) # right diagonal
+    if check_equality(@board[0][2], @board[1][1], @board[2][0]) # right diagonal
       return true
     end
     false
+  end
+
+  def check_equality(first, second, third)
+    (first == second) && (second == third) ? true : false
   end
 
   def display_board
