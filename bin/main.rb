2a828ce32fe92675a891fbd8ff1ba53a3a9fefcb
Urchmaney <kingsobino@gmail.com>
2019-08-08 23:35:20 +0000

    Linter : Add spaces and code editing

--- bin/main.rb
+++ bin/main.rb
@@ -1,19 +1,20 @@
+# frozen_string_literal: true
+
 require '../lib/game'
 
 def start_game
   game = Game.new
   game.display_board
-  while game.space_used < 9    
+  while game.space_used < 9
     print "Input your move player #{game.player}   :"
     move = gets.chomp
     game.make_move(move.to_s)
     game.display_board
-    if game.check_for_win(game.player)
-      puts "Congratulations player 1 you have won." if game.player == 2
-      puts "Congratulations player 2 you have won." if game.player == 1
+    unless !game.check_for_win(game.player)
+      puts game.player == 2 ? 'Congratulations player 1 you have won.' : 'Congratulations player 2 you have won.'
       break
     end
   end
-  puts "The game is over!!!"
+  puts 'The game is over!!!'
 end
 start_game
--- lib/game.rb
+++ lib/game.rb
@@ -1,53 +1,47 @@
 # frozen_string_literal: true
 
 class Game #:nodoc:
-    attr_reader :space_used
-    attr_reader :player
-    def initialize
-      @board = [%w[_ _ _], %w[_ _ _], %w[_ _ _]]
-      @player = 1
-      @space_used = 0
-      @moves_player1 = ""
-      @moves_player2 = ""
-    end
-  
-    def check_for_win(player_flag)
-      winners = ["123","456","789","147","258","369","159","357"]
-      if player_flag == 1
-        moves = @moves_player2
-      else
-        moves = @moves_player1
+  attr_reader :space_used
+  attr_reader :player
+  def initialize
+    @board = [%w[_ _ _], %w[_ _ _], %w[_ _ _]]
+    @player = 1
+    @space_used = 0
+    @moves_player1 = ''
+    @moves_player2 = ''
+  end
+
+  def check_for_win(player_flag)
+    winners = %w[123, 456, 789, 147, 258, 369, 159, 357]
+    moves = player_flag == 1 ? @moves_player2 : @moves_player1
+    moves = moves[moves.length - 4, moves.length - 1] if moves.length > 3
+    winners.include?(moves)
+  end
+
+  def display_board
+    @board.each_with_index do |row, row_index|
+      row_string = ' '
+      row.each_with_index do |element, index|
+        row_string += ' | ' if index != 0
+        row_string += element
       end
-      if moves.length > 3
-        moves = moves[moves.length - 4, moves.length - 1]
-      end
-      winners.include?(moves)
-    end
-  
-    def display_board
-      @board.each_with_index do |row, row_index|
-        row_string=" "
-        row.each_with_index do |element, index|
-          row_string += " | " if index != 0
-          row_string += element
-        end
-        puts row_string
-        puts "-----------" if row_index != 2
-      end
-    end
-  
-    def make_move(index)
-      @space_used += 1
-      if @player == 1
-        @board[(index.to_i-1)/3.floor][(index.to_i-1)%3] = "X"
-        @moves_player1 += index
-        @player = 2
-      else
-        @board[(index.to_i-1)/3.floor][(index.to_i-1)%3] = "O"
-        @moves_player2 += index
-        @player = 1
-      end
+      puts row_string
+      puts '-----------' if row_index != 2
     end
   end
-  # game = Game.new
-  # puts game.check_for_win
+
+  def make_move(index)
+    @space_used += 1
+    if @player == 1
+      @board[(index.to_i - 1) / 3.floor][(index.to_i - 1) % 3] = 'X'
+      @moves_player1 += index
+      @player = 2
+    else
+      @board[(index.to_i - 1) / 3.floor][(index.to_i - 1) % 3] = 'O'
+      @moves_player2 += index
+      @player = 1
+    end
+  end
+end
+# game = Game.new
+# puts game.check_for_win
