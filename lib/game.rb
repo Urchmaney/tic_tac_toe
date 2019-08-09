7e18fe3d8f5a57bdc767b2040cd5b6999d36fb02
fatima ahmed <fatymah001@gmail.com>
2019-08-10 01:49:22 +0300

    implementation of all test cases

--- lib/game.rb
+++ lib/game.rb
@@ -1,8 +1,5 @@
 # frozen_string_literal: true
 
-require '../lib/board'
-require '../lib/player'
-
 class Game #:nodoc:
   attr_reader :space_used
   attr_reader :player1
@@ -32,6 +29,32 @@ class Game #:nodoc:
     @board.space_used += 1
     @board.register_play(((index.to_i - 1) / 3.floor), ((index.to_i - 1) % 3), player.letter_assigned)
     player.moves += index
+  end
+end
+
+class Player #:nodoc:
+  attr_reader :letter_assigned
+  attr_accessor :moves
+  attr_reader :name
+
+  def initialize(name, letter_assigned)
+    @name = name
+    @letter_assigned = letter_assigned
+    @moves = ''
+  end
+end
+
+class Board #:nodoc:
+  attr_reader :array
+  attr_accessor :space_used
+
+  def initialize
+    @array = [%w[1 2 3], %w[4 5 6], %w[7 8 9]]
+    @space_used = 0
+  end
+
+  def register_play(row, column, letter)
+    @array[row][column] = letter
   end
 end
 # game = Game.new
