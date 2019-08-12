26660ad4123ed2346c308fca2669aa524eee8172
Urchmaney <kingsobino@gmail.com>
2019-08-12 21:27:20 +0000

    Stickler : remove semi-colon

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -8,15 +8,15 @@ RSpec.describe Board do
   describe '#register_play' do
     it 'Register letter to the position specified in a two dimensional array.' do
       letter = 'X'
-      row = 1;
-      column = 1;
+      row = 1
+      column = 1
       board.register_play(row, column, letter)
       expect(board.array[row][column]).to eql(letter)
     end
     it 'Does not register another letter in the position specified in a two dimensional array.' do
-      letter = 'O';
-      row = 2;
-      column = 2;
+      letter = 'O'
+      row = 2
+      column = 2
       board.register_play(row, column, letter)
       expect(board.array[row][column]).not_to eql('X')
     end
